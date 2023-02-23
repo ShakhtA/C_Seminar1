c558b429d70d46397d74a3ba09e497b4969ed66e
Andrey <shakht_a@mail.ru>
2023-02-23 21:35:17 +0300

    second task

---
+++ Example1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число - ");
+//string userInputStr = Console.ReadLine() ?? "";
+//int userNumber = int.Parse(userInputStr);
+
+int userNumber = int.Parse(Console.ReadLine() ?? ""); // int.Parse преобразует строковую переменную в целочисленную
+
+int result = (int)Math.Pow(userNumber, 2);  //(int) явное преобразование в тип int
+// библиотека Math возвращает все в вещественном типе double
+
+Console.WriteLine($"{userNumber} -> {result}"); // вывод строки, заключенной в ""
+Console.WriteLine(userNumber + " -> " + result);
---
+++ Example2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число - ");
+int numA = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите целое число - ");
+int numB = int.Parse(Console.ReadLine() ?? "");
+
+if (numA == (numB*numB))
+    {
+        Console.WriteLine("Да");
+    }
+else 
+    {
+        Console.WriteLine("Нет");
+    }
+
+Console.Write("Введите целое число - ");
+int numA = int.Parse(Console.ReadLine() ?? "");
+
+if ((numA < 1) or
